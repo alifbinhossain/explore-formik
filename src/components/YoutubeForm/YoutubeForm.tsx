ad0f596900d1f2aa25bc6b1563bbea16322f3855
alifbinhossain <alifahmed.47@gmail.com>
2022-06-23 20:19:30 +0600

    first commit

---
+++ src/components/YoutubeForm/YoutubeForm.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const YoutubeForm: React.FC = () => {
+  return <div className='h-screen bg-purple-50'>Hello</div>;
+};
+
+export default YoutubeForm;
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import YoutubeForm from '../components/YoutubeForm/YoutubeForm';
+
+export default function Home() {
+  return (
+    <div className=''>
+      <YoutubeForm />
+    </div>
+  );
+}
